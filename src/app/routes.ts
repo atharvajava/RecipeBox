f04d0a5ed93f5d8de4cd2788f4d19758a1ebe559
atharvajava <atharvajava@gmail.com>
2017-04-20 02:39:34 +0530

    finish

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,8 +3,8 @@ import { CreateRecipeComponent } from './create-recipe/create-recipe.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { EditComponent } from './edit/edit.component';
 export const appRoutes:Routes=[
-    { path: '/RecipeBox/create-recipe', component: CreateRecipeComponent },
-    { path: '/RecipeBox/recipe-book', component: RecipeBookComponent},
-    { path: '/RecipeBox/edit-recipe', component: EditComponent},
-    { path: '', redirectTo: '/RecipeBox/recipe-book', pathMatch: 'full' }
+    { path: 'create-recipe', component: CreateRecipeComponent },
+    { path: 'recipe-book', component: RecipeBookComponent},
+    { path: 'edit-recipe', component: EditComponent},
+    { path: '', redirectTo: '/recipe-book', pathMatch: 'full' }
 ];
